eaa593598e05157b9a765a1f399983d34c690308
Pratyush Thota <pratyushthota@Pratyushs-MacBook-Pro.local>
2020-07-24 18:56:24 +0530

    Updates

---
+++ precache-manifest.ce4c3d00523b591e4aba2c1e80f671c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c645ccd9b1c3c3b672fd0aab6bc48d",
+    "url": "/React-Redux-Calculator/index.html"
+  },
+  {
+    "revision": "3ba607f6dbff17ea8d9d",
+    "url": "/React-Redux-Calculator/static/css/main.5a59a19a.chunk.css"
+  },
+  {
+    "revision": "cf532ff3c17667d42488",
+    "url": "/React-Redux-Calculator/static/js/2.77d44154.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Redux-Calculator/static/js/2.77d44154.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ba607f6dbff17ea8d9d",
+    "url": "/React-Redux-Calculator/static/js/main.b51f321a.chunk.js"
+  },
+  {
+    "revision": "721804867324cb394fa9",
+    "url": "/React-Redux-Calculator/static/js/runtime-main.7578f79d.js"
+  }
+]);
